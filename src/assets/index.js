8f16f6f0f80723d486705569ee45af0ec6e18a78
SurajPhulara <suraj66171@gmail.com>
2023-07-16 08:37:18 +0530

    modified responsive bits of code

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,3 +29,4 @@ export { default as twitter } from "./twitter.svg";
 export { default as facebook} from "./facebook.svg";
 export { default as linkedin} from "./linkedin.svg";
 export { default as youtube } from "./youtube.svg";
+export { default as menuIcon} from "./menuIcon.svg";
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import { profileIcon, shoppingCartIcon } from '../../assets';
+import { profileIcon, shoppingCartIcon, menuIcon } from '../../assets';
 import Logo from '../Logo/Logo';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleNavLinkClick = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <div className='navbar'>
       <Logo />
       <div className="navbar__right font-secondary">
         <div className="navbar__links">
-          <NavLink exact to="/" activeClassName="active">Home</NavLink>
-          <NavLink to="/shop">Shop</NavLink>
-          <NavLink to="/collections">Collections</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/" onClick={handleNavLinkClick}>Home</NavLink>
+          <NavLink to="/shop" onClick={handleNavLinkClick}>Shop</NavLink>
+          <NavLink to="/collections" onClick={handleNavLinkClick}>Collections</NavLink>
+          <NavLink to="/about" onClick={handleNavLinkClick}>About</NavLink>
+          <NavLink to="/contact" onClick={handleNavLinkClick}>Contact</NavLink>
         </div>
         <div className="navbar__icons">
-<div className="navbar__icons">
-  <NavLink to="/profile" activeClassName="active" className='navbar__icon'>
-    <img src={profileIcon} alt="profileIcon"  />
-  </NavLink>
-  <NavLink to="/cart" activeClassName="active" className='navbar__icon' >
-    <img src={shoppingCartIcon} alt="shoppingCartIcon"/>
-  </NavLink>
-</div>        </div>
+          <NavLink to="/profile" className='navbar__icon' onClick={handleNavLinkClick}> <img src={profileIcon} alt="profileIcon" /> </NavLink>
+          <NavLink to="/cart" className='navbar__icon' onClick={handleNavLinkClick}> <img src={shoppingCartIcon} alt="shoppingCartIcon" /> </NavLink>
+          <div className="navbar__menu-toggle" onClick={handleMenuToggle}>
+            <img src={menuIcon} alt="menuIcon" />
+          </div>
+        </div>
       </div>
+      {isMenuOpen && (
+        <div className="navbar__dropdown">
+          <div className="navbar_dropdown_container">
+
+          <NavLink to="/" onClick={handleNavLinkClick}>Home</NavLink>
+          <NavLink to="/shop" onClick={handleNavLinkClick}>Shop</NavLink>
+          <NavLink to="/collections" onClick={handleNavLinkClick}>Collections</NavLink>
+          <NavLink to="/about" onClick={handleNavLinkClick}>About</NavLink>
+          <NavLink to="/contact" onClick={handleNavLinkClick}>Contact</NavLink>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
